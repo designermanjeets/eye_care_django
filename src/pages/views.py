af558829b92c228a8f8f8a336343cb4df85fbc6c
AiINFOX <aiinfox@AiINFOXs-MacBook-Pro.local>
2024-07-27 13:55:13 +0530

    return other

--- src/pages/views.py
+++ src/pages/views.py
@@ -1654,10 +1654,10 @@ def handle_user_query1(request,user_query):
   #If the intent is not related to booking, generate a response using the fine-tuned model
     else:
         response = generate_response(user_query)
-        return response
-        # data = json.loads(request.body.decode('utf-8'))
-        # data =data.get('practice_name', '')
-        # return f"Sorry, I can only help with {data} related questions. If you have any inquiries about our services, appointment scheduling, or eye care in general, feel free to ask!"
+        # return response
+        data = json.loads(request.body.decode('utf-8'))
+        data =data.get('practice_name', '')
+        return f"Sorry, I can only help with {data} related questions. If you have any inquiries about our services, appointment scheduling, or eye care in general, feel free to ask!"
 
 # Main function to handle user query
 # def handle_user_query_me(request, user_query):
